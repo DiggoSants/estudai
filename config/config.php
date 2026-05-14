df94ef6ae9919b1759d65f4e943aa8183e9daf6d
DiggoSants <diegoofc135@gmail.com>
2026-05-13 22:14:21 -0300

    configs para o deploy

--- config/config.php
+++ config/config.php
@@ -3,14 +3,14 @@
 declare(strict_types=1);
 
 define('APP_NAME', 'Estudai');
-define('APP_URL', 'http://localhost/estudai');
+define('APP_URL', rtrim($_ENV['APP_URL'] ?? getenv('APP_URL') ?: 'http://localhost/estudai', '/'));
 define('SESSION_NAME', 'estudai_sess');
 
-define('DB_HOST', 'localhost');
-define('DB_PORT', 3306);
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'estudai');
+define('DB_HOST', $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?: 'localhost');
+define('DB_PORT', (int) ($_ENV['DB_PORT'] ?? getenv('DB_PORT') ?: 3306));
+define('DB_USER', $_ENV['DB_USER'] ?? getenv('DB_USER') ?: 'root');
+define('DB_PASS', $_ENV['DB_PASS'] ?? getenv('DB_PASS') ?: '');
+define('DB_NAME', $_ENV['DB_NAME'] ?? getenv('DB_NAME') ?: 'estudai');
 define('DB_CHARSET', 'utf8mb4');
 
 if (session_status() === PHP_SESSION_NONE) {
